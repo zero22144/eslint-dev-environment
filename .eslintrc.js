e9307370ceea5df0cc682650e495519369f4d98b
zero22144 <zero22144@users.noreply.github.com>
2020-11-21 00:15:07 -0500

    Added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": 0,
+        "no-undef": "off"
+    }
+};
